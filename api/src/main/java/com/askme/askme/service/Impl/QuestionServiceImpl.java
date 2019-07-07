7c3d4f9a571b86a76f3c7535ce0e03cbd0fe9ccc
jovicailievski <jovicailievski97@gmail.com>
2019-07-07 20:45:32 +0200

    question post endpoint

--- api/src/main/java/com/askme/askme/service/Impl/QuestionServiceImpl.java
+++ api/src/main/java/com/askme/askme/service/Impl/QuestionServiceImpl.java
@@ -1,10 +1,15 @@
 package com.askme.askme.service.Impl;
 
+import com.askme.askme.exceptions.CategoryNotFoundException;
 import com.askme.askme.exceptions.QuestionNotFoundException;
+import com.askme.askme.exceptions.UserNotFoundException;
 import com.askme.askme.models.Category;
 import com.askme.askme.models.Question;
+import com.askme.askme.models.User;
 import com.askme.askme.repository.jpa.JpaCategoryRepository;
 import com.askme.askme.repository.jpa.JpaQuestionRepository;
+import com.askme.askme.repository.jpa.JpaUserRepository;
+import com.askme.askme.repository.jpa.JpaUserRoleRepository;
 import com.askme.askme.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +22,30 @@ public class QuestionServiceImpl implements QuestionService {
 
     @Autowired
     private JpaQuestionRepository questionRepository;
+
+    @Autowired
+    private JpaCategoryRepository categoryRepository;
+
+    @Autowired
+    private JpaUserRepository userRepository;
+
+    @Override
+    public Question save(Question q) {
+        Optional<Category> oc = categoryRepository.findById(q.getCategory().getId());
+        if(!oc.isPresent()){
+            throw new CategoryNotFoundException();
+        }
+        q.setCategory(oc.get());
+
+        Optional<User> ou = userRepository.findById(q.getUser().getId());
+        if(!ou.isPresent()){
+            throw new UserNotFoundException();
+        }
+        q.setUser(ou.get());
+
+
+        return questionRepository.save(q);
+    }
 
     public List<Question> findAll(){
         return  questionRepository.findAll();
